58f58a7d336e6975a3065e1cee80f9f4a5cc942c
KevinLai02 <jihn.smith629@gmail.com>
2024-06-18 19:19:19 +0800

    add eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 import { useEffect, useState } from "react";
-import * as io from 'socket.io-client';
-import { values } from 'lodash';
-import Error from "next/error";
-
+import * as io from "socket.io-client";
+import React from "react";
 const socket = io.connect("http://localhost:8080/");
 
 // ice server's configuration
 const configuration = {
   iceServers: [
-      {
-          urls: 'stun:stun.l.google.com:19302',
-      }, {
-          urls: 'stun:stun.xten.com',
-      }],
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+    {
+      urls: "stun:stun.xten.com",
+    },
+  ],
 };
 
 const signalOption = {
@@ -21,15 +21,15 @@ const signalOption = {
   offerToReceiveVideo: 1, // 是否傳送影像流給對方
 };
 
-const room =  "testRoom";
+const room = "testRoom";
 
 export default function Home() {
-  const [localStream, setLocalStream] = useState<MediaStream>()
-  const [remote, setRemote] = useState<MediaStream>()
-  const [pcPeers, setPcPeers] = useState<RTCPeerConnection>()
+  const [localStream, setLocalStream] = useState<MediaStream>();
+  const [remote, setRemote] = useState<MediaStream>();
+  const [pcPeers, setPcPeers] = useState<RTCPeerConnection>();
   const join = async () => {
-      socket.emit('join', room);
-      createPeerConnection()
+    socket.emit("join", room);
+    createPeerConnection();
   };
 
   const hangup = () => {
@@ -42,7 +42,7 @@ export default function Home() {
     setPcPeers(undefined);
 
     // 傳遞掛斷事件給 Server
-    socket.emit('hangup', room);
+    socket.emit("hangup", room);
 
     // 移除遠端 video src
     setRemote(undefined); // 移除遠端媒體串流
@@ -52,123 +52,125 @@ export default function Home() {
     // 設定 iceServer
     // 建立 RTCPeerConnection
     const peers: RTCPeerConnection = new RTCPeerConnection(configuration);
-    setPcPeers(peers)
+    setPcPeers(peers);
     // 增加本地串流
     localStream?.getTracks().forEach((track) => {
       peers?.addTrack(track, localStream);
     });
 
     // 找尋到 ICE 候選位址後，送去 Server 與另一位配對
-      peers.onicecandidate = (e) => {
-        if (e.candidate) {
-            // 發送 ICE
-            socket.emit('ice_candidate', room, {
-                label: e.candidate.sdpMLineIndex,
-                id: e.candidate.sdpMid,
-                candidate: e.candidate.candidate,
-            });
-        }
+    peers.onicecandidate = (e) => {
+      if (e.candidate) {
+        // 發送 ICE
+        socket.emit("ice_candidate", room, {
+          label: e.candidate.sdpMLineIndex,
+          id: e.candidate.sdpMid,
+          candidate: e.candidate.candidate,
+        });
+      }
     };
 
     // 監聽 ICE 連接狀態
     peers.oniceconnectionstatechange = (e) => {
-        // 若連接已斷，執行掛斷相關動作
-        const peerConnection = e.target as RTCPeerConnection;
-        if (peerConnection.iceConnectionState === 'disconnected') {
-            hangup();
-        }
+      // 若連接已斷，執行掛斷相關動作
+      const peerConnection = e.target as RTCPeerConnection;
+      if (peerConnection.iceConnectionState === "disconnected") {
+        hangup();
+      }
     };
 
     // 監聽是否有媒體串流傳入
     peers.ontrack = (event) => {
       const [stream] = event.streams;
       setRemote(stream);
+    };
   };
-  };
-    
+
   const createLocalStream = async () => {
     try {
       const constraints = { audio: true, video: true };
 
       // getUserMedia 取得本地影音串流
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      setLocalStream(stream)
+      setLocalStream(stream);
     } catch (err) {
-      console.log('getUserMedia error: ', err);
+      console.log("getUserMedia error: ", err);
     }
   };
 
-    const sendSDP = async (type: string) => {
-      try {
-          if (!pcPeers) {
-              console.log('尚未開啟視訊');
-              return;
-          }
-  
-          const method = type === 'offer' ? 'createOffer' : 'createAnswer';
-          const offerOptions = {
-              offerToReceiveAudio: true, // 是否傳送聲音流給對方
-              offerToReceiveVideo: true // 是否傳送影像流給對方
-          };
-  
-          // 建立 SDP
-          const localSDP = await pcPeers?.[method](offerOptions);
-  
-          // 設定本地 SDP
-          await pcPeers?.setLocalDescription(localSDP);
-  
-          // 發送 SDP
-          socket.emit(type, room, pcPeers?.localDescription);
-      } catch (err) {
-          console.log('error: ', err);
+  const sendSDP = async (type: string) => {
+    try {
+      if (!pcPeers) {
+        console.log("尚未開啟視訊");
+        return;
       }
+
+      const method = type === "offer" ? "createOffer" : "createAnswer";
+      const offerOptions = {
+        offerToReceiveAudio: true, // 是否傳送聲音流給對方
+        offerToReceiveVideo: true, // 是否傳送影像流給對方
+      };
+
+      // 建立 SDP
+      const localSDP = await pcPeers?.[method](offerOptions);
+
+      // 設定本地 SDP
+      await pcPeers?.setLocalDescription(localSDP);
+
+      // 發送 SDP
+      socket.emit(type, room, pcPeers?.localDescription);
+    } catch (err) {
+      console.log("error: ", err);
+    }
   };
 
-    useEffect(() => {
-      createLocalStream()
-        // 監聽加入房間
-      socket.on('ready', (msg) => {
-        // 發送 Offer SDP
-        sendSDP('offer');
-      });
+  useEffect(() => {
+    createLocalStream();
+    // 監聽加入房間
+    socket.on("ready", (msg) => {
+      // 發送 Offer SDP
+      sendSDP("offer");
+    });
 
-      // 監聽收到 Offer
-      socket.on('offer', async (desc) => {
-          // 設定對方的媒體串流
-          await pcPeers?.setRemoteDescription(desc);
-          // 發送 Answer SDP
-          await sendSDP('answer');
-      });
+    // 監聽收到 Offer
+    socket.on("offer", async (desc) => {
+      // 設定對方的媒體串流
+      await pcPeers?.setRemoteDescription(desc);
+      // 發送 Answer SDP
+      await sendSDP("answer");
+    });
 
-      // 監聽收到 Answer
-      socket.on('answer', (desc) => {
-          // 設定對方的媒體串流
-          pcPeers?.setRemoteDescription(desc)
-      });
+    // 監聽收到 Answer
+    socket.on("answer", (desc) => {
+      // 設定對方的媒體串流
+      pcPeers?.setRemoteDescription(desc);
+    });
 
-      // 監聽收到 ICE 候選位址
-      socket.on('ice_candidate', (data) => {
-          // RTCIceCandidate 用以定義 ICE 候選位址
-          const candidate = new RTCIceCandidate({
-              sdpMLineIndex: data.label,
-              candidate: data.candidate
-          });
-          // 加入 ICE 候選位址
-          pcPeers?.addIceCandidate(candidate);
+    // 監聽收到 ICE 候選位址
+    socket.on("ice_candidate", (data) => {
+      // RTCIceCandidate 用以定義 ICE 候選位址
+      const candidate = new RTCIceCandidate({
+        sdpMLineIndex: data.label,
+        candidate: data.candidate,
       });
-    }, [pcPeers])
-    console.log(remote);
-    
+      // 加入 ICE 候選位址
+      pcPeers?.addIceCandidate(candidate);
+    });
+  }, [pcPeers]);
+
   return (
     <div>
       <div>
-        <button onClick={() => {
-            join()
-        }}>Join
+        <button
+          onClick={() => {
+            join();
+          }}
+        >
+          Join
         </button>
       </div>
-      <video 
-        ref={video => {
+      <video
+        ref={(video) => {
           if (video && localStream) {
             video.srcObject = localStream;
           }
@@ -179,17 +181,16 @@ export default function Home() {
       />
       {remote && (
         <video
-          ref={video => {
+          ref={(video) => {
             if (video) {
               video.srcObject = remote;
             }
           }}
+          controls
           autoPlay
-          playsInline
-          muted={true}
+          muted
         />
-        )
-      }
+      )}
     </div>
   );
 }
